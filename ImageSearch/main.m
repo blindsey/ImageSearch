a45a42c551deb26886d422d4c5909cebdedbb6ab
Ben Lindsey <ben.lindsey@gmail.com>
2013-10-25 14:55:19 -0700

    Initial Commit

---
+++ ImageSearch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ImageSearch
+//
+//  Created by Ben Lindsey on 10/25/13.
+//  Copyright (c) 2013 Ben Lindsey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
